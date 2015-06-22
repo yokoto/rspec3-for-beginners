5821fcdc68c81675a30739ce0f48c3e79d0025e9
yokoto <youkotoushi@gmail.com>
2015-06-22 23:20:51 +0900

    add first spec

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -1,5 +1,9 @@
 require_relative 'message_filter'
 
 describe MessageFilter do
+	it 'detects message with NG word' do
+		filter = MessageFilter.new('foo')
+		expect(filter.detect?('hello from foo')).to eq true
+	end
 end
 
